6fae86455a0a4df819f0d9dae640f7a2d4bbfede
scotcy <kimjh470@naver.com>
2023-11-17 19:41:32 +0900

    231117_min
    두줄삭제

--- 19teamGroupAssignment/Program.cs
+++ 19teamGroupAssignment/Program.cs
@@ -7,8 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            
             
         }
     }
