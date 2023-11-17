b3ff430af7bae589ae9df636fdfd3b0235a89e48
[Tanha] <[julus8238@gmail.com]>
2023-11-17 19:34:09 +0900

    ssss

--- 19teamGroupAssignment/Program.cs
+++ 19teamGroupAssignment/Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
